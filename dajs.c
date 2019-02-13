289b86861034279fbc346412569e6934ef1b0b6f
Beppsi <46752177+Beppsi@users.noreply.github.com>
2019-02-13 09:36:56 +0100

    Commenting and fixing readability of the code

--- dajs.c
+++ dajs.c
@@ -14,52 +14,55 @@ printf("would you like to roll the dice?\n");
 scanf("%s", &ans);
 
 
-while (strcmp(ans, "yes") == 0)
+while (strcmp(ans, "yes") == 0) //Startar en loop som låter spelet fortsätta så länge du svarar "yes"
 {
-     if (saldo < 1)
-{
-     printf("You Don't Have Any Money in Saldo, Thanks For Playing!\n");
-     return 0;
-}
-else {
- srand(time(NULL));
- diceNum = rand() % 5;
- dice = diceNum + 1;
- printf("On what number would you like to bet?\n");
- scanf("%d", &guessd);
-  if (guessd < 1 || guessd > 6 )
-  {
-      printf("guess a number between 1 and 6!\n");
-      printf("Guess again?\n");
-      scanf("%s", &ans);
-  }
+     if (saldo < 1) //Avslutar spelet om saldo är tommt
+        {
+             printf("You Don't Have Any Money in Saldo, Thanks For Playing!\n");
+             return 0;
+        }
+    else
+    {   
+     srand(time(NULL)); //Random Nummer för tärningen
+     diceNum = rand() % 5;
+     dice = diceNum + 1;
+     printf("On what number would you like to bet?\n");
+     scanf("%d", &guessd); //Läser in gissning
 
-  else {
- printf("how much money do you wanna bet on number %d ? \n", guessd);
- scanf("%d", &bet);
- printf("you guessed: %d \n", guessd);
- printf("The dice says : %d \n", dice);
+  if (guessd < 1 || guessd > 6 ) //Du kan inte gissa ett nummer som inte finns på tärningen
+    {
+        printf("guess a number between 1 and 6!\n");
+        printf("Guess again?\n");
+        scanf("%s", &ans);
+    }
 
- if (guessd == dice)
-  {
-   saldo = saldo + bet ;
-   printf("You won!\n saldo is now %d \n", saldo);
+  else //Här börjar själva spelet om du har pengar och väljer ett nummer som finns
+      {
+      printf("how much money do you wanna bet on number %d ? \n", guessd);
+      scanf("%d", &bet); //Läser satsning
+      printf("you guessed: %d \n", guessd);
+      printf("The dice says : %d \n", dice);
 
-  }
- else if (guessd != dice)
-  {
-   saldo = saldo - bet ;
-   printf("You've lost!\n saldo is now %d \n", saldo);
-   printf("would you like to roll the dice again?\n");
-   scanf("%s", &ans);
-  }
+         if (guessd == dice) //Vinst
+            {
+              saldo = saldo + bet ;
+              printf("You won!\n saldo is now %d \n", saldo);
+
+            }
+            else if (guessd != dice) //Förlust
+            {
+             saldo = saldo - bet ;
+             printf("You've lost!\n saldo is now %d \n", saldo);
+             printf("would you like to roll the dice again?\n");
+             scanf("%s", &ans);
+            }
+          }
+    }
 }
-}
-}
-while (strcmp(ans, "yes") != 0)
+while (strcmp(ans, "yes") != 0) //Avslutar spelet om du inte svarar "yes"
  {
   printf("Fair enough, see ya around!\n");
-  printf("You ended the game with %d in saldo", saldo);
+  printf("You ended the game with %d in saldo", saldo); //Skriver ut ditt slutliga saldo
   return 0;
  }
  return 0;
